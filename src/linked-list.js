f58dfa18dca79199a277f1900afc1a36dffeef79
ManGysITI <mangysiti@gmail.com>
2017-10-01 23:52:03 +0300

    in the process of writing

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,21 +1,70 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        let node = new Node(data);
 
-    head() {}
+        if (this.length){
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        else{
+            this._head = node;
+            this._tail = node;
+        }
+        this.length++;
+        return node;
+    }
 
-    tail() {}
+    head() {
+        return this._head.data;
+    }
+
+    tail() {
+        return this._head.data;
+    }
 
     at(index) {}
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        var node = new Node(data);
+        if (index < 0 || (index > this.length - 1 && this.length !== 0)) {
+            return false;
+        }
 
-    isEmpty() {}
+        if (index === 0) {
+            if (this.isEmpty()) {
+                this.append(data);
+                return this;
+            } else {
+                node.next = this._head;
+                this._head.prev = node;
+                this._head = node;
+                this.length++;
+            }
+            return this;
 
-    clear() {}
+
+    }
+    }
+
+    isEmpty() {
+        return this.length == 0 ? true : false;
+    }
+
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
 
     deleteAt(index) {}
 
